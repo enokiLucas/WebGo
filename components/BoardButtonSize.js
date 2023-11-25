f4f9501d53aa7b286e0eb613c768ceb69cf2dda0
Lucas Enoki <enoki.lucas@gmail.com>
2023-11-25 17:10:01 -0300

    Change the style of the page

---
+++ components/BoardButtonSize.js
@@ -0,0 +1,30 @@
+class BoardButtonSize extends HTMLElement {
+	constructor() {
+		super(); // Always call super() first in a Web Component constructor.
+
+		// Attach a shadow root to the element.
+		this.attachShadow({ mode: 'open' });
+
+		// Set up the initial state or properties
+		this.boardSize = this.getAttribute('board-size') || 9; // Default to 9x9
+	}
+
+	connectedCallback() {
+		const button = document.createElement('button');
+		button.textContent = `${this.boardSize}x${this.boardSize} Board`;
+
+		// Add event listener
+		button.addEventListener('click', () => {
+			// Emit a custom event with the board size
+			this.dispatchEvent(new CustomEvent('board-create', {
+				detail: { size: this.boardSize },
+				bubbles: true, // Allows the event to bubble up through the DOM
+			}));
+		});
+
+		this.shadowRoot.appendChild(button);
+	}
+}
+
+// Define the custom element
+customElements.define('board-button-size', BoardButtonSize);
