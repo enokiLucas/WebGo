9bff4a5fe2ca53af345f401bf58502580f83587b
Lucas Enoki <enoki.lucas@gmail.com>
2023-11-27 09:54:16 -0300

    Create 'setupBoardInteractions' and related methods.
    Based on 'addListenersToBoard' from app.js

--- components/GoBoard.js
+++ components/GoBoard.js
@@ -21,7 +21,7 @@ class GoBoard extends HTMLElement {
 	connectedCallback() {
 		// Called when the element is inserted into the DOM
 		this.initializeBoard(this.boardSize);
-		//addListenersToBoard(this, this.boardSize);
+		this.setupBoardInteractions();
 
 		// Listen for 'board-create' event on the document
 		document.addEventListener('board-create', (event) => {
@@ -164,8 +164,76 @@ class GoBoard extends HTMLElement {
 		this.shadowRoot.appendChild(board)
 	} //End of the render function
 
-// Additional methods related to board functionality
-// For example, method to update board state, handle user interactions, etc.
+	// Additional methods related to board functionality
+
+	setupBoardInteractions() {
+		const svgNS = "http://www.w3.org/2000/svg";
+
+		// Initialize the ghost piece on the board
+		this.initializeGhostPiece('black');
+		const ghostStone = this.shadowRoot.getElementById('ghostStone');
+
+		// Iterate over the grid size to access each intersection
+		for (let i = 0; i < this.boardSize; i++) {
+			for (let j = 0; j < this.boardSize; j++) {
+				// Create a circle at each intersection for the event listener
+				let intersection = document.createElementNS(svgNS, "circle");
+				intersection.setAttribute('cx', EDGE_MARGIN + (i * LENGTH_SQUARE));
+				intersection.setAttribute('cy', EDGE_MARGIN + (j * LENGTH_SQUARE));
+				intersection.setAttribute('r', 10); // Small radius, essentially invisible
+				intersection.setAttribute('fill', 'transparent'); // Make the circle invisible
+				intersection.setAttribute('class', 'intersection');
+
+				// Add Ghost Stone locked into the listener
+				this.setupIntersectionListeners(intersection, ghostStone);
+
+				// Add click event listener to place a stone on the board
+				intersection.addEventListener('click', (event) => {
+					this.saveClick(event);
+				});
+
+				// Append the intersection to the board
+				this.shadowRoot.appendChild(intersection);
+			}
+		}
+	} //end of setupBoardInteractions
+
+	//Methods used by setupBoardInteractions().
+	initializeGhostPiece(color) {
+		const svgNS = "http://www.w3.org/2000/svg";
+		let ghostStone = document.createElementNS(svgNS, "circle");
+		ghostStone.setAttribute('id', 'ghostStone');
+		ghostStone.setAttribute('r', LENGTH_SQUARE / 2); // The radius should be half the square size
+		ghostStone.setAttribute('fill', color); // Placeholder color
+		ghostStone.setAttribute('fill-opacity', '0.5'); // Semi-transparent
+		ghostStone.style.visibility = 'hidden'; // Initially hidden
+		this.shadowRoot.appendChild(ghostStone);
+	}
+
+	setupIntersectionListeners(intersection, ghostStone) {
+		intersection.addEventListener('mouseenter', () => {
+			ghostStone.setAttribute('cx', intersection.getAttribute('cx'));
+			ghostStone.setAttribute('cy', intersection.getAttribute('cy'));
+			ghostStone.style.visibility = 'visible';
+		});
+
+		intersection.addEventListener('mouseleave', () => {
+			ghostStone.style.visibility = 'hidden';
+		});
+	}
+
+	saveClick(event) {
+		let x = event.target.cx.baseVal.value;
+		let y = event.target.cy.baseVal.value;
+
+		let boardX = (x - EDGE_MARGIN) / LENGTH_SQUARE;
+		let boardY = (y - EDGE_MARGIN) / LENGTH_SQUARE;
+
+		console.log(`Intersection clicked at: (${ALPHABET[boardX]}, ${this.boardSize - boardY})`);
+		console.log(`Intersection at [${boardX}, ${boardY}]`);
+	}
+
+
 } //End of the class
 
 // Define the custom element
