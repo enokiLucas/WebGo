03fc428a002dcf2d6ba9ae35bbe6f549bd709fe2
Lucas Enoki <enoki.lucas@gmail.com>
2023-12-05 17:08:31 -0300

    Change approach towards the 'BoardListeners'
    Create 'EventListeners.js' to add the event listeners to the intersections.
    Create 'GameStateManager.js' to handle the state of the game, positions, points, etc.
    Create 'GhostPieceManager.js' to manage the ghost piece.

---
+++ components/EventListeners.js
@@ -0,0 +1,4 @@
+export function addEventListeners(board, ghostPieceManager, gameStateManager) {
+    // Logic to add event listeners
+    // These listeners can interact with ghostPieceManager and gameStateManager
+}
---
+++ components/GhostPieceManager.js
@@ -0,0 +1,4 @@
+class GhostPieceManager extends HTMLElement {
+    // Custom element logic for the ghost piece
+}
+customElements.define('ghost-piece-manager', GhostPieceManager);
