b989c01155c293b0461de75e4fc5234ce59caa71
Lucas Enoki <enoki.lucas@gmail.com>
2023-12-06 08:54:09 -0300

    import global constants and change the name of variables.
    The variables were given more generic names so to be used in more places.

--- components/EventListeners.js
+++ components/EventListeners.js
@@ -1,9 +1,11 @@
-export function addEventListeners(board, ghostPieceManager, gameStateManager) {
+import { EDGE_MARGIN, LENGTH_SQUARE} from '../utils/constants.js'
+
+export function addEventListeners(board, boardSize, onIntersectionHover, onIntersectionClick) {
 	// Logic to add event listeners
 	// These listeners can interact with ghostPieceManager and gameStateManager
 	// Iterate over the grid size to access each intersection
-	for (let i = 0; i < this.boardSize; i++) {
-		for (let j = 0; j < this.boardSize; j++) {
+	for (let i = 0; i < boardSize; i++) {
+		for (let j = 0; j < boardSize; j++) {
 			// Create a circle at each intersection for the event listener
 			let intersection = document.createElementNS(SVG_NS, "circle");
 			intersection.setAttribute('cx', EDGE_MARGIN + (i * LENGTH_SQUARE));
@@ -11,6 +13,8 @@ export function addEventListeners(board, ghostPieceManager, gameStateManager) {
 			intersection.setAttribute('r', 10); // Small radius, essentially invisible
 			intersection.setAttribute('fill', 'transparent'); // Make the circle invisible
 			intersection.setAttribute('class', 'intersection');
+
+			board.appendChild(intersection);
 		}
 	}
 }
