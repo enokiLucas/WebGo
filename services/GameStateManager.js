220dea824349bf81529ed06444e1fe97fc1ae347
Lucas Enoki <enoki.lucas@gmail.com>
2023-12-10 14:57:58 -0300

    Start working on the manager modules

--- services/GameStateManager.js
+++ services/GameStateManager.js
@@ -1,3 +1,21 @@
+// GameStateManager.js
 export class GameStateManager {
-    // Methods for managing game state
+	constructor() {
+		this.gameState = {
+			// Define the initial state of the game
+			// e.g., board state, current player, etc.
+		};
+	}
+
+	makeMove(intersection) {
+		// Logic to update the game state based on a move
+		// 'intersection' can be used to determine the move details
+	}
+
+	getCurrentState() {
+		// Return the current game state
+		return this.gameState;
+	}
+
+	// Add other methods as needed for game state management
 }
