a076fd71ec3368be4613dc7685424668a9e2c5f4
Lucas Enoki <enoki.lucas@gmail.com>
2023-12-11 22:20:52 -0300

    so far only handles the ghost piece

--- services/HandleIntersectionHover.js
+++ services/HandleIntersectionHover.js
@@ -1,6 +1,10 @@
-import { updateGhostPiece } from './GhostPieceManager.js';
+import { EDGE_MARGIN, LENGTH_SQUARE } from '../utils/constants.js';
 
-export function handleIntersectionHover(intersection) {
-	// Logic for hover interaction
-	updateGhostPiece(intersection);
+export function handleIntersectionHover(ghostPiece, event) {
+	const intersection = event.target;
+	const x = intersection.cx.baseVal.value;
+	const y = intersection.cy.baseVal.value;
+
+	// Optionally, snap the ghost piece to the center of the intersection
+	ghostPiece.show(x, y);
 }
