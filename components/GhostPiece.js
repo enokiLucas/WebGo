c9fb8f7a04c45a33413e27b63e13e9178d7bc092
Lucas Enoki <enoki.lucas@gmail.com>
2023-12-16 16:27:45 -0300

    added an svg element over the ghost-piece circle

--- components/GhostPiece.js
+++ components/GhostPiece.js
@@ -9,19 +9,28 @@ class GhostPiece extends HTMLElement {
 	}
 
 	createGhostPiece() {
+		const svg = document.createElementNS(SVG_NS, 'svg');
+		svg.setAttribute('width', '100%');
+		svg.setAttribute('height', '100%');
+
 		this.ghostPiece = document.createElementNS(SVG_NS, "circle");
 		this.ghostPiece.setAttribute('r', LENGTH_SQUARE / 2); // Radius of the ghost piece
 		this.ghostPiece.setAttribute('fill', 'black'); // Semi-transparent fill
 		this.ghostPiece.setAttribute('fill-opacity', '0.5'); // Adjust for desired transparency
 		this.ghostPiece.style.visibility = 'hidden'; //Initially hidden
 
-		this.shadowRoot.appendChild(this.ghostPiece);
+		svg.appendChild(this.ghostPiece);
+		this.shadowRoot.appendChild(svg);
+
+		console.log('hello createGhostPiece');
 	}
 
 	show(x, y) {
 		this.ghostPiece.setAttribute('cx', x);
 		this.ghostPiece.setAttribute('cy', y);
 		this.ghostPiece.style.visibility = 'visible';
+
+		console.log('hello show');
 	}
 
 	hide() {
