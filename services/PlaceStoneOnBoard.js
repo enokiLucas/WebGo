55dc8fe91a5f6a1e37270d37528803c4404e9e51
Lucas Enoki <enoki.lucas@gmail.com>
2024-02-11 20:27:56 -0300

    Added attributes to the stones based on coordinates

--- services/PlaceStoneOnBoard.js
+++ services/PlaceStoneOnBoard.js
@@ -6,5 +6,14 @@ export function placeStoneOnBoard(board, x, y, playerColor) {
 	stone.setAttribute('cy', y);
 	stone.setAttribute('r', LENGTH_SQUARE / 2);
 	stone.setAttribute('fill', playerColor);
+
+	// Convert coordinates relative to the boardMatrix.
+	const boardX = (x - EDGE_MARGIN) / LENGTH_SQUARE;
+	const boardY = (y - EDGE_MARGIN) / LENGTH_SQUARE;
+
+	// Add data attributes to store the board coordinates
+	stone.setAttribute('data-x', boardX);
+	stone.setAttribute('data-y', boardY);
+
 	board.appendChild(stone);
 }
