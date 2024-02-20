0e907e7821e67847d35709d3d078821b83f8b6ac
Lucas Enoki <enoki.lucas@gmail.com>
2024-02-20 12:27:23 -0300

    Updated GameStateManager

--- services/GameStateManager.js
+++ services/GameStateManager.js
@@ -36,32 +36,32 @@ class GameStateManager { //Remember to fix SGF
 		return this.currentPlayer;
 	}
 
-	addMove(coordinates, metadata = {}) { //Go to handle Intersection Click and change the module
+	recordMove(x, y, metadata = {}) { //Go to handle Intersection Click and change the module
 		const move = {
 			branch: 0, // Default branch
 			key: this.moveKey++
 			player: this.currentPlayer,
-			coordinates: coordinates,
+			x: x,
+			y: y,
 			metadata: metadata
 		};
 		this.movesHistory.push(move);
-		this.togglePlayer();
 	}
 /*
 	getSGFMoves() {
 		return this.moves.map(move => `;${move.color}[${move.position}]`).join('');
 	}
 */
-	makeMove(color, position) {
+	makeMove(x, y) {
 		// Logic to handle a move
-		this.addMove(color, position);
+		this.recordMove(x, y);
 		this.togglePlayer();
 		const event = new CustomEvent('moveMade', { detail: { currentPlayer: this.currentPlayer } });
 		document.dispatchEvent(event);
 	}
 
-	makePass(color) {
-		this.addMove(null, { pass: true }); // Add a pass move movesHistory
+	makePass() {
+		this.recordMove(null, { pass: true }); // Add a pass move movesHistory
 		this.togglePlayer(); // Switch turns
 	}
 }
