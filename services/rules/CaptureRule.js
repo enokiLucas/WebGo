6af1a7ad76881a127b31459277203e2893898e0a
Lucas Enoki <enoki.lucas@gmail.com>
2024-02-28 16:08:09 -0300

    Adapt capureRule to the new format

--- services/rules/CaptureRule.js
+++ services/rules/CaptureRule.js
@@ -6,6 +6,24 @@ import { identifyGroups, hasLiberties } from '../../utils/RulesUtil.js';
 class CaptureRule {
 	constructor() {}
 
+	// Method to analyze potential captures without removing stones
+	analyzeCaptures(x, y, color) {
+		const potentialCaptures = [];
+		// Identify opposite color groups around the last move
+		const oppositeColorGroups = identifyGroups(x, y, true);
+
+		oppositeColorGroups.forEach(group => {
+			// Check if the group has no liberties
+			if (!hasLiberties(group)) {
+				// Instead of removing, add group to potential captures
+				potentialCaptures.push(...group);
+			}
+		});
+
+		return potentialCaptures; // Return all groups that would be captured
+	}
+
+	// Method to remove a group of stones from the board
 	removeGroup(board, group) {
 		group.forEach(stone => {
 			// Update the boardMatrix
@@ -18,38 +36,18 @@ class CaptureRule {
 				stoneElement.remove(); // Removes the SVG element from the DOM
 			}
 		});
-	}
 
 
-	// Wrapper function to be called after a move is made
-	processCaptures(board, x, y, color) {
-		// Identify opposite color groups around the last move
-		const oppositeColorGroups = identifyGroups(x, y, true);
-
-		// Check each group for captures
-		oppositeColorGroups.forEach(group => {
-			// console.log(group);
-			// Check if the group has no liberties
-			if (!hasLiberties(group)) {
-
-				// Remove the captured group from the board
-				this.removeGroup(board, group);
-
-				// Dispatch an event with metadata about the capture
-				const metadata = {
-					type: 'capture',
-					captured_stones_color: color,
-					stones: group
-				};
-				const captureEvent = new CustomEvent('new-metadata', {
-					detail: metadata
-				});
-				document.dispatchEvent(captureEvent);
-
-			}
+		// Dispatch an event with metadata about the capture to handleIntersectionClick
+		const metadata = {
+			type: 'capture',
+			captured_stones_color: color,
+			stones: group
+		};
+		const captureEvent = new CustomEvent('new-metadata', {
+			detail: metadata
 		});
-
-	// Optionally, check for suicide (not implemented here)
+		document.dispatchEvent(captureEvent);
 	}
 
 }
