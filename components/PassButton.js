2ace0940c9ad9acdfbcc0afb8b30e6e1c4337319
Lucas Enoki <enoki.lucas@gmail.com>
2024-03-15 10:51:26 -0300

    Remove obsolete code blocks

--- components/PassButton.js
+++ components/PassButton.js
@@ -6,7 +6,6 @@ class PassButton extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
-		//this.initializeButton();
 	}
 
 	connectedCallback() {
@@ -24,13 +23,6 @@ class PassButton extends HTMLElement {
 */
 		this.shadowRoot.appendChild(button);
 	}
-
-	/*initializeButton() {
-		const button = document.createElement('button');
-		button.textContent = 'Pass';
-		button.addEventListener('click', gameStateManager.makePass());
-		this.shadowRoot.appendChild(button);
-	}*/
 }
 
 customElements.define('pass-button', PassButton);
