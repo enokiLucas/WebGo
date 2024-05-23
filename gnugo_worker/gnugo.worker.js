0f31e5e60550983f95417552febb429d3dc2be89
Lucas Enoki <enoki.lucas@gmail.com>
2024-05-23 16:37:49 -0300

    Format TTY

--- gnugo_worker/gnugo.worker.js
+++ gnugo_worker/gnugo.worker.js
@@ -12,8 +12,10 @@
 // after the generated code, you will need to define   var Module = {};
 // before the code. Then that object will be used in the code, and you
 // can continue to use Module afterwards as well.
+
 //var Module = typeof Module != 'undefined' ? Module : {};
-var Module = {
+
+var Module = { // CUSTOM Module
 	// Specify GTP mode as a command-line argument
 	arguments: ['--mode', 'gtp'],
 
@@ -1917,147 +1919,148 @@ var tempI64;
       }
       return FS_stdin_getChar_buffer.shift();
     };
-  var TTY = {
-  ttys:[],
-  init() {
-        // https://github.com/emscripten-core/emscripten/pull/1555
-        // if (ENVIRONMENT_IS_NODE) {
-        //   // currently, FS.init does not distinguish if process.stdin is a file or TTY
-        //   // device, it always assumes it's a TTY device. because of this, we're forcing
-        //   // process.stdin to UTF8 encoding to at least make stdin reading compatible
-        //   // with text files until FS.init can be refactored.
-        //   process.stdin.setEncoding('utf8');
-        // }
-      },
-  shutdown() {
-        // https://github.com/emscripten-core/emscripten/pull/1555
-        // if (ENVIRONMENT_IS_NODE) {
-        //   // inolen: any idea as to why node -e 'process.stdin.read()' wouldn't exit immediately (with process.stdin being a tty)?
-        //   // isaacs: because now it's reading from the stream, you've expressed interest in it, so that read() kicks off a _read() which creates a ReadReq operation
-        //   // inolen: I thought read() in that case was a synchronous operation that just grabbed some amount of buffered data if it exists?
-        //   // isaacs: it is. but it also triggers a _read() call, which calls readStart() on the handle
-        //   // isaacs: do process.stdin.pause() and i'd think it'd probably close the pending call
-        //   process.stdin.pause();
-        // }
-      },
-  register(dev, ops) {
-        TTY.ttys[dev] = { input: [], output: [], ops: ops };
-        FS.registerDevice(dev, TTY.stream_ops);
-      },
-  stream_ops:{
-  open(stream) {
-          var tty = TTY.ttys[stream.node.rdev];
-          if (!tty) {
-            throw new FS.ErrnoError(43);
-          }
-          stream.tty = tty;
-          stream.seekable = false;
-        },
-  close(stream) {
-          // flush any pending line data
-          stream.tty.ops.fsync(stream.tty);
-        },
-  fsync(stream) {
-          stream.tty.ops.fsync(stream.tty);
-        },
-  read(stream, buffer, offset, length, pos /* ignored */) {
-          if (!stream.tty || !stream.tty.ops.get_char) {
-            throw new FS.ErrnoError(60);
-          }
-          var bytesRead = 0;
-          for (var i = 0; i < length; i++) {
-            var result;
-            try {
-              result = stream.tty.ops.get_char(stream.tty);
-            } catch (e) {
-              throw new FS.ErrnoError(29);
-            }
-            if (result === undefined && bytesRead === 0) {
-              throw new FS.ErrnoError(6);
-            }
-            if (result === null || result === undefined) break;
-            bytesRead++;
-            buffer[offset+i] = result;
-          }
-          if (bytesRead) {
-            stream.node.timestamp = Date.now();
-          }
-          return bytesRead;
-        },
-  write(stream, buffer, offset, length, pos) {
-          if (!stream.tty || !stream.tty.ops.put_char) {
-            throw new FS.ErrnoError(60);
-          }
-          try {
-            for (var i = 0; i < length; i++) {
-              stream.tty.ops.put_char(stream.tty, buffer[offset+i]);
-            }
-          } catch (e) {
-            throw new FS.ErrnoError(29);
-          }
-          if (length) {
-            stream.node.timestamp = Date.now();
-          }
-          return i;
-        },
-  },
-  default_tty_ops:{
-  get_char(tty) {
-          return FS_stdin_getChar();
-        },
-  put_char(tty, val) {
-          if (val === null || val === 10) {
-            out(UTF8ArrayToString(tty.output, 0));
-            tty.output = [];
-          } else {
-            if (val != 0) tty.output.push(val); // val == 0 would cut text output off in the middle.
-          }
-        },
-  fsync(tty) {
-          if (tty.output && tty.output.length > 0) {
-            out(UTF8ArrayToString(tty.output, 0));
-            tty.output = [];
-          }
-        },
-  ioctl_tcgets(tty) {
-          // typical setting
-          return {
-            c_iflag: 25856,
-            c_oflag: 5,
-            c_cflag: 191,
-            c_lflag: 35387,
-            c_cc: [
-              0x03, 0x1c, 0x7f, 0x15, 0x04, 0x00, 0x01, 0x00, 0x11, 0x13, 0x1a, 0x00,
-              0x12, 0x0f, 0x17, 0x16, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            ]
-          };
-        },
-  ioctl_tcsets(tty, optional_actions, data) {
-          // currently just ignore
-          return 0;
-        },
-  ioctl_tiocgwinsz(tty) {
-          return [24, 80];
-        },
-  },
-  default_tty1_ops:{
-  put_char(tty, val) {
-          if (val === null || val === 10) {
-            err(UTF8ArrayToString(tty.output, 0));
-            tty.output = [];
-          } else {
-            if (val != 0) tty.output.push(val);
-          }
-        },
-  fsync(tty) {
-          if (tty.output && tty.output.length > 0) {
-            err(UTF8ArrayToString(tty.output, 0));
-            tty.output = [];
-          }
-        },
-  },
-  };
+
+		var TTY = {
+			ttys:[],
+			init() {
+				// https://github.com/emscripten-core/emscripten/pull/1555
+				// if (ENVIRONMENT_IS_NODE) {
+				//   // currently, FS.init does not distinguish if process.stdin is a file or TTY
+				//   // device, it always assumes it's a TTY device. because of this, we're forcing
+				//   // process.stdin to UTF8 encoding to at least make stdin reading compatible
+				//   // with text files until FS.init can be refactored.
+				//   process.stdin.setEncoding('utf8');
+				// }
+			},
+			shutdown() {
+				// https://github.com/emscripten-core/emscripten/pull/1555
+				// if (ENVIRONMENT_IS_NODE) {
+				//   // inolen: any idea as to why node -e 'process.stdin.read()' wouldn't exit immediately (with process.stdin being a tty)?
+				//   // isaacs: because now it's reading from the stream, you've expressed interest in it, so that read() kicks off a _read() which creates a ReadReq operation
+				//   // inolen: I thought read() in that case was a synchronous operation that just grabbed some amount of buffered data if it exists?
+				//   // isaacs: it is. but it also triggers a _read() call, which calls readStart() on the handle
+				//   // isaacs: do process.stdin.pause() and i'd think it'd probably close the pending call
+				//   process.stdin.pause();
+				// }
+			},
+			register(dev, ops) {
+				TTY.ttys[dev] = { input: [], output: [], ops: ops };
+				FS.registerDevice(dev, TTY.stream_ops);
+			},
+			stream_ops:{
+				open(stream) {
+					var tty = TTY.ttys[stream.node.rdev];
+					if (!tty) {
+						throw new FS.ErrnoError(43);
+					}
+					stream.tty = tty;
+					stream.seekable = false;
+				},
+				close(stream) {
+					// flush any pending line data
+					stream.tty.ops.fsync(stream.tty);
+				},
+				fsync(stream) {
+					stream.tty.ops.fsync(stream.tty);
+				},
+				read(stream, buffer, offset, length, pos /* ignored */) {
+					if (!stream.tty || !stream.tty.ops.get_char) {
+						throw new FS.ErrnoError(60);
+					}
+					var bytesRead = 0;
+					for (var i = 0; i < length; i++) {
+						var result;
+						try {
+							result = stream.tty.ops.get_char(stream.tty);
+						} catch (e) {
+							throw new FS.ErrnoError(29);
+						}
+						if (result === undefined && bytesRead === 0) {
+							throw new FS.ErrnoError(6);
+						}
+						if (result === null || result === undefined) break;
+						bytesRead++;
+						buffer[offset+i] = result;
+					}
+					if (bytesRead) {
+						stream.node.timestamp = Date.now();
+					}
+					return bytesRead;
+				},
+				write(stream, buffer, offset, length, pos) {
+					if (!stream.tty || !stream.tty.ops.put_char) {
+						throw new FS.ErrnoError(60);
+					}
+					try {
+						for (var i = 0; i < length; i++) {
+							stream.tty.ops.put_char(stream.tty, buffer[offset+i]);
+						}
+					} catch (e) {
+						throw new FS.ErrnoError(29);
+					}
+					if (length) {
+						stream.node.timestamp = Date.now();
+					}
+					return i;
+				},
+			},
+			default_tty_ops:{
+				get_char(tty) {
+					return FS_stdin_getChar();
+				},
+				put_char(tty, val) {
+					if (val === null || val === 10) {
+						out(UTF8ArrayToString(tty.output, 0));
+						tty.output = [];
+					} else {
+						if (val != 0) tty.output.push(val); // val == 0 would cut text output off in the middle.
+					}
+				},
+				fsync(tty) {
+					if (tty.output && tty.output.length > 0) {
+						out(UTF8ArrayToString(tty.output, 0));
+						tty.output = [];
+					}
+				},
+				ioctl_tcgets(tty) {
+					// typical setting
+					return {
+						c_iflag: 25856,
+						c_oflag: 5,
+						c_cflag: 191,
+						c_lflag: 35387,
+						c_cc: [
+							0x03, 0x1c, 0x7f, 0x15, 0x04, 0x00, 0x01, 0x00, 0x11, 0x13, 0x1a, 0x00,
+							0x12, 0x0f, 0x17, 0x16, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+							0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+						]
+					};
+				},
+				ioctl_tcsets(tty, optional_actions, data) {
+					// currently just ignore
+					return 0;
+				},
+				ioctl_tiocgwinsz(tty) {
+					return [24, 80];
+				},
+			},
+			default_tty1_ops:{
+				put_char(tty, val) {
+					if (val === null || val === 10) {
+						err(UTF8ArrayToString(tty.output, 0));
+						tty.output = [];
+					} else {
+						if (val != 0) tty.output.push(val);
+					}
+				},
+				fsync(tty) {
+					if (tty.output && tty.output.length > 0) {
+						err(UTF8ArrayToString(tty.output, 0));
+						tty.output = [];
+					}
+				},
+			},
+		};
 
 
   var zeroMemory = (address, size) => {
@@ -7414,6 +7417,7 @@ function onMessageFromMainEmscriptenThread(message) {
 				console.log('Custom message received but worker Module.onCustomMessage not implemented.');
       }*/
 			////////////////START CUSTOM LOGIC
+			/*
 			var inputQueue = [];
 			inputQueue = inputQueue.concat(message.data.data.split('').map(c => c.charCodeAt(0)));
 
@@ -7425,7 +7429,8 @@ function onMessageFromMainEmscriptenThread(message) {
 				return inputQueue.shift();
 				console.log('1111');
 			};
-
+*/
+			default_tty_ops.read(message.data.data);
 			///////////////END CUSTOM LOGIC
       break;
     }
