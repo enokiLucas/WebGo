dc4a742d9c757db7df780a53f93e0850daee3893
Lucas Enoki <enoki.lucas@gmail.com>
2024-06-08 08:53:08 -0300

    Fix emscript
    The module opens on GTP mode now

--- gnugo2/emscript.js
+++ gnugo2/emscript.js
@@ -23,16 +23,7 @@ var Module = {
 						}
 				};
 		})(),
-		canvas: (() => {
-				var canvas = document.getElementById('canvas');
 
-				// As a default initial behavior, pop up an alert when webgl context is lost. To make your
-				// application robust, you may want to override this behavior before shipping!
-				// See http://www.khronos.org/registry/webgl/specs/latest/1.0/#5.15.2
-				canvas.addEventListener("webglcontextlost", (e) => { alert('WebGL context lost. You will need to reload the page.'); e.preventDefault(); }, false);
-
-				return canvas;
-		})(),
 		setStatus: (text) => {
 				if (!Module.setStatus.last) Module.setStatus.last = { time: Date.now(), text: '' };
 				if (text === Module.setStatus.last.text) return;
@@ -41,19 +32,6 @@ var Module = {
 				if (m && now - Module.setStatus.last.time < 30) return; // if this is a progress update, skip it if too soon
 				Module.setStatus.last.time = now;
 				Module.setStatus.last.text = text;
-				if (m) {
-						text = m[1];
-						progressElement.value = parseInt(m[2])*100;
-						progressElement.max = parseInt(m[4])*100;
-						progressElement.hidden = false;
-						spinnerElement.hidden = false;
-				} else {
-						progressElement.value = null;
-						progressElement.max = null;
-						progressElement.hidden = true;
-						if (!text) spinnerElement.style.display = 'none';
-				}
-				statusElement.innerHTML = text;
 		},
 		totalDependencies: 0,
 		monitorRunDependencies: (left) => {
