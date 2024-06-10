2e859762334948238e19dfcadcd00d412c1b2798
Lucas Enoki <enoki.lucas@gmail.com>
2024-06-10 20:15:13 -0300

    fix territoryScoring

--- services/score/territoryScoring.js
+++ services/score/territoryScoring.js
@@ -1,33 +1,47 @@
-//Japanese rules
 import { rulesControl } from '../RulesControl.js';
-import { gameStateManager } from '../GameStateManager.js';
+import { exploreTerritory } from './ScoreUtil.js';
 
 class TerritoryScoring {
 	constructor() {
-		this.scoringBoard = rulesControl.createSimulatedBoardMatrix();
-		this.blackTerritory = 0;
-		this.whiteTerritory = 0;
-	}
-
-	countScore() {
-		// Example logic to count territories
-		for (let x = 0; x < this.scoringBoard.length; x++) {
-			for (let y = 0; y < this.scoringBoard[x].length; y++) {
-				if (this.scoringBoard[x][y] === 'black') {
-					this.blackTerritory++;  // Count occupied territories for black
-				} else if (this.scoringBoard[x][y] === 'white') {
-					this.whiteTerritory++;  // Count occupied territories for white
-				}
-				// Add logic for counting empty surrounded territories
-			}
-		}
-		// Log the scores for debugging
-		console.log(`Black Territory: ${blackTerritory}, White Territory: ${whiteTerritory}`);
+		this.scoringBoard = rulesControl.createSimulatedBoardMatrix(); // Assuming this creates a deep copy of the board
+		this.resetTerritoriesCount();
 	}
 
 	resetTerritoriesCount() {
 		this.blackTerritory = 0;
 		this.whiteTerritory = 0;
+		this.visited = new Set();
+	}
+
+	countScore() {
+		for (let x = 0; x < this.scoringBoard.length; x++) {
+			for (let y = 0; y < this.scoringBoard[x].length; y++) {
+				if (this.scoringBoard[x][y] === null && !this.visited.has(`${x},${y}`)) {
+					const { territory, isCompletelySurrounded, surroundedBy } = exploreTerritory(x, y, this.scoringBoard);
+					if (isCompletelySurrounded) {
+						// Mark all explored territory points as visited
+						territory.forEach(point => {
+							this.visited.add(`${point.x},${point.y}`);
+						});
+						// Add territory size to the corresponding player's score
+						if (surroundedBy === 'black') {
+							this.blackTerritory += territory.length;
+						} else if (surroundedBy === 'white') {
+							this.whiteTerritory += territory.length;
+						}
+					}
+				}
+			}
+		}
+		console.log(`Black Territory: ${this.blackTerritory}, White Territory: ${this.whiteTerritory}`);
+	}
+
+	// Optional: Method to get current scores
+	getScores() {
+		return {
+			black: this.blackTerritory,
+			white: this.whiteTerritory
+		};
 	}
 }
 
