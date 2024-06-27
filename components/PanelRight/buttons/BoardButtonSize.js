3d7ee2d7b183fa2ddc9a4410ee4822745409b6ca
Lucas Enoki <enoki.lucas@gmail.com>
2024-06-27 09:36:13 -0300

    GameControlPanelV2 >> PanelRight

---
+++ components/PanelRight/buttons/BoardButtonSize.js
@@ -0,0 +1,33 @@
+import { loadStyles } from '../../../utils/StyleLoader.js';
+import { gameStateManager } from '../../../services/GameStateManager.js'
+
+class BoardButtonSize extends HTMLElement {
+	constructor() {
+		super(); // Always call super() first in a Web Component constructor.
+
+		// Attach a shadow root to the element.
+		this.attachShadow({ mode: 'open' });
+
+		// Set up the initial state or properties
+		this.boardSize = this.getAttribute('board-size')
+	}
+
+	connectedCallback() {
+		loadStyles(this.shadowRoot, '../../../assets/styles/Buttons.css');
+
+		const button = document.createElement('button');
+		button.textContent = `${this.boardSize}x${this.boardSize} Board`;
+
+		// Add event listener
+		button.addEventListener('click', () => {
+			gameStateManager.boardSize = this.boardSize;
+		});
+
+		this.shadowRoot.appendChild(button);
+
+	}
+
+}
+
+// Define the custom element
+customElements.define('board-button-size', BoardButtonSize);
